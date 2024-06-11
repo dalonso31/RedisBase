95f4778924eb8058cfd905740ec0f4ed706f1677
Daniela Alonso <alonsod2000@gmail.com>
2024-06-10 22:04:26 -0700

    Added commands to server

--- server.cpp
+++ server.cpp
@@ -10,10 +10,12 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <string>
 #include <vector>
-#include <stdbool.h>
+#include <map>
 
 const size_t k_max_msg = 4096;
+const size_t k_max_args = 1024;
 
 enum {
     STATE_REQ = 0,
@@ -64,6 +66,109 @@ static void fd_set_nb(int fd) {
 static void state_req(Conn *conn);
 static void state_res(Conn *conn);
 
+static int32_t parse_req(
+    const uint8_t *data, size_t len, std::vector<std::string> &out)
+{
+    if (len < 4) {
+        return -1;
+    }
+    uint32_t n = 0;
+    memcpy(&n, &data[0], 4);
+    if (n > k_max_args) {
+        return -1;
+    }
+
+    size_t pos = 4;
+    while (n--) {
+        if (pos + 4 > len) {
+            return -1;
+        }
+        uint32_t sz = 0;
+        memcpy(&sz, &data[pos], 4);
+        if (pos + 4 + sz > len) {
+            return -1;
+        }
+        out.push_back(std::string((char *)&data[pos + 4], sz));
+        pos += 4 + sz;
+    }
+
+    if (pos != len) {
+        return -1;  // trailing garbage
+    }
+    return 0;
+}
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+static std::map<std::string, std::string> g_map;
+
+static uint32_t do_get(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    if (!g_map.count(cmd[1])) {
+        return RES_NX;
+    }
+
+    std::string &val = g_map[cmd[1]];
+    assert(val.size() <= k_max_msg);
+    memcpy(res, val.data(), val.size());
+    *reslen = (uint32_t)val.size();
+    return RES_OK;
+}
+
+static uint32_t do_set(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map[cmd[1]] = cmd[2];
+    return RES_OK;
+}
+
+static uint32_t do_del(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map.erase(cmd[1]);
+    return RES_OK;
+}
+
+static bool cmd_is(const std::string &word, const char *cmd) {
+    return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+static int32_t do_request(
+    const uint8_t *req, uint32_t reqlen,
+    uint32_t *rescode, uint8_t *res, uint32_t *reslen)
+{
+    std::vector<std::string> cmd;
+    if (0 != parse_req(req, reqlen, cmd)) {
+        msg("bad req");
+        return -1;
+    }
+
+    if (cmd.size() == 2 && cmd_is(cmd[0],"get")){
+        *rescode = do_get(cmd, res, reslen);
+    } else if (cmd.size() == 2 && cmd_is(cmd[0],"set")){
+        *rescode = do_set(cmd, res, reslen);
+    }else if (cmd.size() == 2 && cmd_is(cmd[0],"del")){
+        *rescode = do_del(cmd, res, reslen);
+    } else {
+        // Send error response
+        *rescode = RES_ERR;
+        const char *msg = "Unknown command";
+        strcpy((char *)res, msg);
+        *reslen = strlen(msg);
+        return 0;
+    }
+    return 0;
+}
+
 static bool try_one_request(Conn *conn) {
     // try to parse a request from the buffer
     if (conn->rbuf_size < 4) {
@@ -82,10 +187,21 @@ static bool try_one_request(Conn *conn) {
         return false;
     }
 
-    // got one request, do something with it
-    printf("client says: %.*s\n", len, &conn->rbuf[4]);
+    // got one request, generate the response
+    uint32_t rescode = 0;
+    uint32_t wlen = 0;
 
-    // generating echoing response
+    int32_t err = do_request(
+        &conn->rbuf[4], len, 
+        &rescode, &conn->wbuf[4 + 4], &wlen
+    );
+
+    if (err){
+        conn->state = STATE_END;
+        return false;
+    }
+
+    wlen += 4;
     memcpy(&conn->wbuf[0], &len, 4);
     memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
     conn->wbuf_size = 4 + len;
